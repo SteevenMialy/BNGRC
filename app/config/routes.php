a4650e1a7f0d9268b014749a16234478b300d285
RAKOTOMANGA Mialy Steeven <rakotomangamialysteeven@gmail.com>
2026-02-16 23:17:45 +0300

    routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -5,6 +5,7 @@ use app\controllers\BesoinController;
 use app\controllers\VilleController;
 use app\controllers\DonsController;
 use app\controllers\SimulationController;
+use app\controllers\AchatController;
 use app\middlewares\SecurityHeadersMiddleware;
 use app\models\Besoin;
 use app\models\Category;
@@ -54,13 +55,43 @@ $router->group('', function (Router $router) use ($app) {
 	});
 
 	$router->get('/simulation/@idville/@iddon/@qte/@taux', function ($idville, $iddon, $qte, $taux) use ($app) {
+		$stockreste = SimulationController::stockreste($iddon, $qte);
+		$argentreste = SimulationController::argentreste($iddon, $qte, $taux);
+		$prix = SimulationController::calculprix($iddon, $qte, $taux);
+
+		// Vérifier si le don est déjà en stock
+		$donDejaEnStock = SimulationController::donDejaEnStock($iddon);
+		// Vérifier si le prix > argent disponible
+		$argentInsuffisant = ($argentreste === false);
+		// La simulation est valide seulement si aucune erreur
+		$simulationValide = !$donDejaEnStock && !$argentInsuffisant;
+
 		$app->render('Simulation', [
-			'prix' => SimulationController::calculprix($iddon, $qte, $taux),
-			'stockreste' => SimulationController::stockreste($iddon, $qte),
-			'argentreste' => SimulationController::argentreste($iddon, $qte,$taux),
+			'prix' => $prix,
+			'stockreste' => $stockreste,
+			'argentreste' => $argentreste,
+			'idville' => $idville,
+			'iddon' => $iddon,
+			'qte' => $qte,
+			'taux' => $taux,
 			'ville' => VilleController::getVilleById($idville),
-			'don' => DonsController::getDonById($iddon)
+			'don' => DonsController::getDonById($iddon),
+			'donDejaEnStock' => $donDejaEnStock,
+			'argentInsuffisant' => $argentInsuffisant,
+			'simulationValide' => $simulationValide
 		]);
+	});
+
+	$router->get('/achat/valider/@idville/@iddon/@qte/@taux', function ($idville, $iddon, $qte, $taux) {
+		AchatController::validerSimulation($idville, $iddon, $qte, $taux);
+	});
+
+	$router->get('/achat/liste', function () {
+		AchatController::listeFiltrable();
+	});
+
+	$router->get('/achat/liste/@idville', function ($idville) {
+		AchatController::listeFiltrable($idville);
 	});
 
 
