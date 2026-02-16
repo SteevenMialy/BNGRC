347cc9d7b22e946ac2b9c48f3d9b97ca6ec79446
RAKOTOMANGA Mialy Steeven <rakotomangamialysteeven@gmail.com>
2026-02-16 21:38:55 +0300

    achat-script

---
+++ script/2026-02-16-Steeven-achat.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE gd_achat (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idVille INT NOT NULL,
+    idDons INT NOT NULL,
+    taux int NOT NULL,
+    quantite DECIMAL(15,2) NOT NULL,
+    date_achat TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (idVille) REFERENCES gd_villes(id),
+    FOREIGN KEY (idTypes) REFERENCES gd_typesDons(id),
+    FOREIGN KEY (idDons) REFERENCES gd_dons(id)
+);
+
+
